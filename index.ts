49baa328078cd1376d34e9435fedc4811618bd82
jameswilddev <jameswilddev@outlook.com>
2021-12-18 11:45:02 +0000

    Add error and warning support.

--- index.ts
+++ index.ts
@@ -15,8 +15,11 @@ import type {
   DefinitionProvider,
   Definition,
   DefinitionLink,
+  Diagnostic,
+  TextEditor,
 } from "vscode";
 import { optionalRequire } from "optional-require";
+import type { Warning, Error } from "@skitscript/types-nodejs";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const vscode = optionalRequire(`vscode`);
@@ -257,7 +260,254 @@ const definitionProvider: DefinitionProvider = {
   },
 };
 
+function convertWarningOrErrorToDiagnostic(
+  warningOrError: Warning | Error
+): Diagnostic {
+  switch (warningOrError.type) {
+    case `duplicateIdentifierInList`:
+      return new vscode.Diagnostic(
+        new vscode.Range(
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.second.fromColumn - 1
+          ),
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.second.toColumn
+          )
+        ),
+        `This item appears more than once in this list; this is likely to be a mistake.`,
+        vscode.DiagnosticSeverity.Warning
+      );
+
+    case `emptyLabel`:
+      return new vscode.Diagnostic(
+        new vscode.Range(
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.label.fromColumn - 1
+          ),
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.label.toColumn
+          )
+        ),
+        `This label immediately leads elsewhere; this is likely to be a mistake.`,
+        vscode.DiagnosticSeverity.Warning
+      );
+
+    case `flagNeverReferenced`:
+      return new vscode.Diagnostic(
+        new vscode.Range(
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.flag.fromColumn - 1
+          ),
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.flag.toColumn
+          )
+        ),
+        `This flag is never used; this is likely to be a mistake.`,
+        vscode.DiagnosticSeverity.Warning
+      );
+
+    case `flagNeverSet`:
+      return new vscode.Diagnostic(
+        new vscode.Range(
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.flag.fromColumn - 1
+          ),
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.flag.toColumn
+          )
+        ),
+        `This flag is never set; this is likely to be a mistake.`,
+        vscode.DiagnosticSeverity.Warning
+      );
+
+    case `inconsistentIdentifier`:
+      return new vscode.Diagnostic(
+        new vscode.Range(
+          new vscode.Position(
+            warningOrError.second.line - 1,
+            warningOrError.second.fromColumn - 1
+          ),
+          new vscode.Position(
+            warningOrError.second.line - 1,
+            warningOrError.second.toColumn
+          )
+        ),
+        `This is written differently earlier in this document; this is likely to be a mistake.`,
+        vscode.DiagnosticSeverity.Warning
+      );
+
+    case `unreachable`:
+      return new vscode.Diagnostic(
+        new vscode.Range(
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.fromColumn - 1
+          ),
+          new vscode.Position(warningOrError.line - 1, warningOrError.toColumn)
+        ),
+        `This line (and every line following until the next label or the end of the file) are impossible to reach, which is likely to be a mistake.`,
+        vscode.DiagnosticSeverity.Warning
+      );
+
+    case `unreferencedLabel`:
+      return new vscode.Diagnostic(
+        new vscode.Range(
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.label.fromColumn - 1
+          ),
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.label.toColumn
+          )
+        ),
+        `This label is never referenced; this is likely to be a mistake.`,
+        vscode.DiagnosticSeverity.Warning
+      );
+
+    case `duplicateLabel`:
+      return new vscode.Diagnostic(
+        new vscode.Range(
+          new vscode.Position(
+            warningOrError.second.line - 1,
+            warningOrError.second.fromColumn - 1
+          ),
+          new vscode.Position(
+            warningOrError.second.line - 1,
+            warningOrError.second.toColumn
+          )
+        ),
+        `This label has the same (normalized) name as another previously declared.`,
+        vscode.DiagnosticSeverity.Error
+      );
+
+    case `incompleteEscapeSequence`:
+      return new vscode.Diagnostic(
+        new vscode.Range(
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.column - 1
+          ),
+          new vscode.Position(warningOrError.line - 1, warningOrError.column)
+        ),
+        `This formatted text ends with a backslash; if you meant to insert a literal backslash, enter two (\\\\).`,
+        vscode.DiagnosticSeverity.Error
+      );
+
+    case `invalidEscapeSequence`:
+      return new vscode.Diagnostic(
+        new vscode.Range(
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.fromColumn - 1
+          ),
+          new vscode.Position(warningOrError.line - 1, warningOrError.toColumn)
+        ),
+        `This pair of characters resemble an escape sequence, but this is not a supported escape sequence; if you meant to insert a literal backslash, enter two (\\\\).`,
+        vscode.DiagnosticSeverity.Error
+      );
+
+    case `undefinedLabel`:
+      return new vscode.Diagnostic(
+        new vscode.Range(
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.label.fromColumn - 1
+          ),
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.label.toColumn
+          )
+        ),
+        `This label has not been declared.`,
+        vscode.DiagnosticSeverity.Error
+      );
+
+    case `unparsable`:
+      return new vscode.Diagnostic(
+        new vscode.Range(
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.fromColumn - 1
+          ),
+          new vscode.Position(warningOrError.line - 1, warningOrError.toColumn)
+        ),
+        `This line's format was not understood.  If it is intended to be dialog, indent it.  Otherwise, ensure that no identifiers include keywords, that the line ends with a full stop and that its grammar is correct.`,
+        vscode.DiagnosticSeverity.Error
+      );
+
+    case `unterminatedBold`:
+      return new vscode.Diagnostic(
+        new vscode.Range(
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.fromColumn - 1
+          ),
+          new vscode.Position(warningOrError.line - 1, warningOrError.toColumn)
+        ),
+        `A run of bold text is started using two asterisks (**) but is never ended.  If you did not intend to start a run of bold text, escape the asterisks with backslashes (\\*\\*).  Otherwise, end the bold text before the end of the line with two asterisks (**).`,
+        vscode.DiagnosticSeverity.Error
+      );
+
+    case `unterminatedCode`:
+      return new vscode.Diagnostic(
+        new vscode.Range(
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.fromColumn - 1
+          ),
+          new vscode.Position(warningOrError.line - 1, warningOrError.toColumn)
+        ),
+        `A run of code is started using a backtick (\`) but is never ended.  If you did not intend to start a run of bold text, escape the backtick with a backslash (\\\`).  Otherwise, end the code before the end of the line with a backtick (\`).`,
+        vscode.DiagnosticSeverity.Error
+      );
+
+    case `unterminatedItalic`:
+      return new vscode.Diagnostic(
+        new vscode.Range(
+          new vscode.Position(
+            warningOrError.line - 1,
+            warningOrError.fromColumn - 1
+          ),
+          new vscode.Position(warningOrError.line - 1, warningOrError.toColumn)
+        ),
+        `A run of italic text is started using an asterisk (*) but is never ended.  If you did not intend to start a run of italic text, escape the asterisk with backslashes (\\*).  Otherwise, end the bold text before the end of the line with an asterisk (*).`,
+        vscode.DiagnosticSeverity.Error
+      );
+  }
+}
+
 export function activate(context: ExtensionContext): void {
+  const diagnosticCollection =
+    vscode.languages.createDiagnosticCollection(`skitscript`);
+
+  const refreshDiagnostics = (document: TextDocument) => {
+    const parsed = parse(document.getText());
+
+    if (parsed.type === `valid`) {
+      diagnosticCollection.set(
+        document.uri,
+        parsed.warnings.map(convertWarningOrErrorToDiagnostic)
+      );
+    } else {
+      diagnosticCollection.set(document.uri, [
+        convertWarningOrErrorToDiagnostic(parsed.error),
+      ]);
+    }
+  };
+
+  if (vscode.window.activeTextEditor) {
+    refreshDiagnostics(vscode.window.activeTextEditor.document);
+  }
+
   context.subscriptions.push(
     vscode.Disposable.from(
       vscode.languages.registerRenameProvider(documentSelector, renameProvider),
@@ -268,7 +518,19 @@ export function activate(context: ExtensionContext): void {
       vscode.languages.registerDefinitionProvider(
         documentSelector,
         definitionProvider
-      )
+      ),
+      diagnosticCollection,
+      vscode.workspace.onDidChangeTextDocument((textEditor: TextEditor) => {
+        refreshDiagnostics(textEditor.document);
+      }),
+      vscode.workspace.onDidCloseTextDocument((document: TextDocument) => {
+        diagnosticCollection.delete(document.uri);
+      }),
+      vscode.window.onDidChangeActiveTextEditor((textEditor?: TextEditor) => {
+        if (textEditor !== undefined) {
+          refreshDiagnostics(textEditor.document);
+        }
+      })
     )
   );
 }
